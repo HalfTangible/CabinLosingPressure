ed090ae8f4866cfa0d6e29e858abe7f3bca80d26
David Burton <HTNetherwing@gmail.com>
2023-08-15 23:15:41 -0500

    Added navmesh, still need to make sure it works

--- Assets/Scripts/Astronaut.cs
+++ Assets/Scripts/Astronaut.cs
@@ -10,22 +10,46 @@ public class Astronaut : MonoBehaviour
     float repairSkill;
     bool haveTools;
     bool canAct;
+    bool isPanicking;
+    bool isMoving;
+
+    Vector2 currentPos;
+    Vector2 targetPos;
+
+    UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        //agent.updateUpAxis = false; //If we add this then the agent can't go up and down. Which is a problem because I NEED THEM TO BE ABLE TO DO SO.
+
         suitBreached = false;
         pressure = 1;
         repairSkill = 1;
         haveTools = false;
         canAct = true;
+        isPanicking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (pressure < .3)
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        //transform.rotation = new Vector3(0, 0, 0);
+
+        if (NeedToMove() && !isPanicking)
+        {
+            Walking();
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        if (pressure < .3 || isPanicking || isMoving)
         {
             canAct = false;
         }
@@ -52,6 +76,23 @@ public class Astronaut : MonoBehaviour
     {
         thisRoom.BeingRepaired(repairSkill);
     }
+
+    void Walking()
+    {
+
+    }
+
+    bool NeedToMove()
+    {
+        return true;
+        //If you're not at your target position or at least close to it, then yes.
+        
+        //Get the differences.
+
+
+    }
+
+
 }
 
 
