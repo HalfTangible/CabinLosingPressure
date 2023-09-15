8d7b9c4b0eb654cb42ba483febb33440a91bfafd
David Burton <HTNetherwing@gmail.com>
2023-09-15 02:19:32 -0500

    Setting up asteroid and DamageController scripts

---
+++ Assets/DamageController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    
+
+    //Handles how damage works.
+    //Asteroids will strike each room, doing damage that needs to be repaired.
+    //Asteroids will be spawned and head towards rooms.
+    //When the asteroid hits, it does damage based on the asteroid's size.
+    
+
+    void Update()
+    {
+
+    }
+
+    void SpawnAsteroid (int timeToHit, int sizeOfRock)
+    {
+    //Instantiate an asteroid prefab with the room as the target.
+    }
+
+}
