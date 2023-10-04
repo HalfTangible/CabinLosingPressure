cfaee24366ee63bea5f2faefcc1cdb35e41e5d39
David Burton <HTNetherwing@gmail.com>
2023-10-04 09:44:55 -0500

    Text added

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters;
 using UnityEngine;
+using TMPro;
 
 public class Room : MonoBehaviour
 {
+    public TextMeshProUGUI damageTextBox;
     public float pressure;
     bool breached; //marks whether the room is open to the void of space;
-    float damage;
+    public float damage;
     // Start is called before the first frame update
     void Start()
     {
+        damageTextBox = GetComponentInChildren<TextMeshProUGUI>();
         pressure = 1;
         breached = false;
     }
@@ -18,6 +21,9 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        damageTextBox.text = "Damage: " + damage.ToString() + "\nAtm: " + pressure;
+
+
         if (breached)
         {
             pressure -= (Time.deltaTime * damage);
